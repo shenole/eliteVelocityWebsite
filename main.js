f00a0bb71e1491fee9390d78eaae19d1ab3c3e24
Shenole Latimer <shenole@gmail.com>
2018-01-15 16:50:26 -0500

    Most of About page is complete. Still need to iron out Flex Box settings

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ openBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
   document.querySelector('nav').classList.remove('openNav');
 })
+
+////////// Slider One ////////
+let i = 0; // starting point of slide shadow
+let images = [];
+let cell = document.querySelector('#slider-one');
+let time = 3500; // interval between slides
+
+////////// Images //////////
+images[0] = 'url(images/slides/slide01.jpg)';
+images[1] = 'url(images/slides/slide02.jpg)';
+images[2] = 'url(images/slides/slide03.jpg)';
+images[3] = 'url(images/slides/slide04.jpg)';
+images[4] = 'url(images/slides/slide05.jpg)';
+
+////////// Function to Change Images //////////
+function chngImg() {
+    cell.style.background = (images[i]);
+    cell.style.backgroundRepeat = ('no-repeat');
+    cell.style.backgroundPosition = ('center');
+    cell.style.backgroundSize = ('cover');
+
+    if(i < images.length - 1) {
+      i++;
+    } else {
+      i = 0; // resets the slide show
+    }
+
+  setTimeout('chngImg()', time);
+}
+
+window.onload = chngImg();
